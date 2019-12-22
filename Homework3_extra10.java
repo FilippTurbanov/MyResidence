3666c1ef81822a86e975a83eb92b2431b5c038ed
Филиппчик <filipp.turbanov@gmal.com>
2019-12-22 13:05:08 +0300

    Homework

---
+++ Homework3_extra10.java
@@ -0,0 +1,54 @@
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Homework3_extra10 {
+    public static void main(String[] args) {
+        // Гласная ли буква
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите букву: ");
+        String letter = in.nextLine();
+        String[] vowels = {"а", "о", "у", "э", "ы", "и", "е", "ё", "ю", "я", "А", "О", "У", "Э", "Ы", "И", "Е", "Ё", "Ю", "Я"};
+        boolean isVowel = false;
+        // 1-й способ
+        for (int i = 0; i <= 19; i++) {
+            if (letter.equalsIgnoreCase(vowels[i])) {
+                System.out.println("Буква гласная.");
+                isVowel = true;
+                break;
+            }
+
+        }
+        if (isVowel == false)
+            System.out.println("Буква согласная.");
+        // 2-й способ
+        isVowel = false;
+        for (int i = 0; i <= 19; i++) {
+            if (Objects.equals(letter, vowels[i])) {
+                System.out.println("Буква гласная.");
+                isVowel = true;
+                break;
+            }
+
+        }
+        if (isVowel == false)
+            System.out.println("Буква согласная.");
+        // 3-й способ
+        String vowels2 = "аоуэыиеёюяАОУЭЫИЕЁЮЯ";
+        isVowel = false;
+        for (int i = 0; i <= 19; i++) {
+            if (letter.charAt(0) == (vowels2.charAt(i))) {
+                System.out.println("Буква гласная.");
+                isVowel = true;
+                break;
+            }
+        }
+        if (isVowel == false)
+            System.out.println("Буква согласная.");
+        // 4-й способ
+        int index = vowels2.indexOf(letter);
+        if (index == -1)
+            System.out.println("Буква согласная.");
+        else
+            System.out.println("Буква гласная.");
+    }
+}
---
+++ Homework3_extra18.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Homework3_extra18 {
+    public static void main(String[] args) {
+        // найти количество различных цифр у заданного натурального числа
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введите натуральное число: ");
+        int number = in.nextInt();
+        int tmpNumber = number;
+        int counter = 1;
+        // выясняем количество цифр в числе
+        while (tmpNumber != 0) {
+            tmpNumber = tmpNumber / 10;
+            if (tmpNumber != 0)
+                counter++;
+        }
+        int counter2 = counter;
+        int compareNumber;
+        String compareList = "";
+        int finalCounter = 0;
+        while (counter2 != 0) {
+            compareNumber = number % ((int)(Math.pow(10, counter2))) / ((int)(Math.pow(10, counter2 - 1)));
+            if (compareList.indexOf(""+compareNumber) == -1)
+                finalCounter ++;
+            compareList += "" + compareNumber;
+            counter2--;
+        }
+        System.out.println("Количество различных цифр в заданном числе " + number + ": " + finalCounter + ".");
+    }
+}
