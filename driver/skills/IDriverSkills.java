74f5fd1267ae4f14dbd2fa6bfb4c4b0699df1c53
FilippTurbanov <58940565+FilippTurbanov@users.noreply.github.com>
2019-12-27 15:31:46 +0300

    Driver

--- driver/skills/IDriverSkills.java
+++ driver/skills/IDriverSkills.java
@@ -5,4 +5,39 @@ public interface IDriverSkills {
     void closeCar();
     void driveCar();
     void takePassenger();
+    void startCar();
+    void stopCar();
+}
+
+class DriverSkills implements IDriverSkills {
+
+    @Override
+    public void openCar() {
+        System.out.println("Открываю двери машины.");
+    }
+
+    @Override
+    public void closeCar() {
+        System.out.println("Закрываю двери.");
+    }
+
+    @Override
+    public void driveCar() {
+        System.out.println("Поехал.");
+    }
+
+    @Override
+    public void takePassenger() {
+        System.out.println("Беру пассажира.");
+    }
+
+    @Override
+    public void startCar() {
+        System.out.println("Завожу мотор.");
+    }
+
+    @Override
+    public void stopCar() {
+        System.out.println("Останавливаюсь.");
+    }
 }
