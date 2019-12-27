6c78ed69bc292fc892fe944c52fef7fd5760cfd9
FilippTurbanov <58940565+FilippTurbanov@users.noreply.github.com>
2019-12-27 15:32:20 +0300

    Driver

--- driver/info/IDriverInfo.java
+++ driver/info/IDriverInfo.java
@@ -5,3 +5,21 @@ public interface IDriverInfo {
     void licenceCategories();
     void typesOfKeys();
 }
+
+class DriverInfo implements IDriverInfo {
+
+    @Override
+    public void drivingExperience() {
+        System.out.println("Стаж вождения составляет 5 лет.");
+    }
+
+    @Override
+    public void licenceCategories() {
+        System.out.println("Имеются права категорий B, C, D");
+    }
+
+    @Override
+    public void typesOfKeys() {
+        System.out.println("Имеются ключи от Audi, Lada, Почтового ящика:)");
+    }
+}
