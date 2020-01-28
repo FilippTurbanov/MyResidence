34e817e9d45e823b953efcac3311d888ebaa96a5
Filipp4ik <filipp.turbanov@gmail.com>
2020-01-28 13:22:07 +0300

    New package for previous homework (28.01.2020)

--- Homework3_2.java
+++
@@ -1,7 +0,0 @@
-public class Homework3_2 {
-    public static void main(String[] args) {
-        String a = "Мама мыла раму";
-        String b = "Мама" + " мыла " + "раму";
-        System.out.println(a == b); // будет true, т.к. и в первом, и во втором случаях мы задаем строку через литерал.
-    }
-}
